125083205c3502dea7584e10c99ae9bd338e4daa
Paulo Dias <paulo.sousa.dias@gmail.com>
2022-04-20 16:28:47 +0100

    Cleanup and added license MIT.

--- theme-changer.js
+++ theme-changer.js
@@ -16,97 +16,76 @@
 //!   <script src="_html/theme-changer.js"></script>
 //!
 //! @author: Paulo Dias
+//! (c) 2022 Paulo Dias
+//! https://opensource.org/licenses/MIT
 
 (() => {
-    const insertThemeToggleElementsIntoThemeTogleElement = (() => {
-      //   <div class="theme-toggle">
-      //     <!-- img id="theme-light-icon" alt="light theme icon" src="_html/light-icon.svg" -->
-      //     <!-- img id="theme-dark-icon" alt="dark theme icon" src="_html/dark-icon.svg" -->
-      //     <label class="theme-toggle-switch" for="theme-toggle-input">
-      //         <!-- button id="theme-toggle-input">Toggle Dark-Mode</button -->
-      //         <input type="checkbox" id="theme-toggle-input" checked=""></input>
-      //         <div class="theme-toggle-switch__control"></div>
-      //     </label>
-      //   </div>
-      //   <script src="_html/theme-changer.js"></script>
-      const containerDiv = document.getElementsByClassName('theme-toggle')[0];
+  const insertThemeToggleElementsIntoThemeTogleElement = (() => {
+    const containerDiv = document.getElementsByClassName('theme-toggle')[0];
 
-      var labelElem = document.createElement('label');
-      labelElem.classList.add('theme-toggle-switch');
-      labelElem.setAttribute('for', 'theme-toggle-input');
+    var labelElem = document.createElement('label');
+    labelElem.classList.add('theme-toggle-switch');
+    labelElem.setAttribute('for', 'theme-toggle-input');
 
-      // var buttonElem = document.createElement('button');
-      // buttonElem.id = 'theme-toggle-input';
-      // buttonElem.appendChild(document.createTextNode('Toggle Dark-Mode'));
+    var checkboxElem = document.createElement('input');
+    checkboxElem.setAttribute('type', 'checkbox');
+    checkboxElem.id = 'theme-toggle-input';
+    checkboxElem.setAttribute('checked', '');
 
-      var checkboxElem = document.createElement('input');
-      checkboxElem.setAttribute('type', 'checkbox');
-      checkboxElem.id = 'theme-toggle-input';
-      checkboxElem.setAttribute('checked', '');
+    var toggleCtlElem = document.createElement('div');
+    toggleCtlElem.classList.add('theme-toggle-switch__control');
 
-      var toggleCtlElem = document.createElement('div');
-      toggleCtlElem.classList.add('theme-toggle-switch__control');
+    labelElem.appendChild(checkboxElem);
+    labelElem.appendChild(toggleCtlElem);
 
-      // labelElem.appendChild(buttonElem);
-      labelElem.appendChild(checkboxElem);
-      labelElem.appendChild(toggleCtlElem);
+    containerDiv.appendChild(labelElem);
+  })
 
-      containerDiv.appendChild(labelElem);
-    })
+  insertThemeToggleElementsIntoThemeTogleElement();
+  const btn = document.getElementById('theme-toggle-input');
 
-    insertThemeToggleElementsIntoThemeTogleElement();
-    const btn = document.getElementById("theme-toggle-input");
+  const osPreference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  const preferredTheme = localStorage.getItem('theme') || osPreference;
 
-    const osPreference = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
-    const preferredTheme = localStorage.getItem('theme') || osPreference;
-    
-    // const sunIcon = document.getElementById('theme-light-icon');
-    // const moonIcon = document.getElementById('theme-dark-icon');
+  var currentTheme = preferredTheme;
 
-    var currentTheme = preferredTheme;
-
-    const setTheme = (theme, persist = true) => {
+  const setTheme =
+      (theme, persist = true) => {
         const htmlElem = document.documentElement;
-        if (theme == "dark") {
-            htmlElem.classList.add("dark");
-            htmlElem.classList.remove("light");
-            btn.checked = true;
+        if (theme == 'dark') {
+          htmlElem.classList.add('dark');
+          htmlElem.classList.remove('light');
+          btn.checked = true;
         } else {
-            htmlElem.classList.remove("dark");
-            htmlElem.classList.add("light");
-            btn.checked = false;
+          htmlElem.classList.remove('dark');
+          htmlElem.classList.add('light');
+          btn.checked = false;
         }
         currentTheme = theme;
 
         if (persist) {
-            localStorage.setItem("theme", theme);
+          localStorage.setItem('theme', theme);
         }
+      }
+
+  const updateUI = (theme) => {}
+
+  btn.addEventListener('click', function() {
+    const htmlElem = document.documentElement;
+    if (currentTheme == 'dark') {
+      htmlElem.classList.remove('dark');
+      htmlElem.classList.add('light');
+    } else {
+      htmlElem.classList.add('dark');
+      htmlElem.classList.remove('light');
     }
+    var theme = currentTheme == 'dark' ? 'light' : 'dark';
+    currentTheme = theme;
+    localStorage.setItem('theme', theme);
 
-    const updateUI = (theme) => {
-        // const isLight = theme === 'dark';
-        // (isLight ? sunIcon : moonIcon).classList.add('active');
-        // (isLight ? moonIcon : sunIcon).classList.remove('active');
-    }
+    updateUI(theme);
+  });
 
-    btn.addEventListener("click", function () {
-        const htmlElem = document.documentElement;
-        if (currentTheme == "dark") {
-            htmlElem.classList.remove("dark");
-            htmlElem.classList.add("light");
-        } else {
-            htmlElem.classList.add("dark");
-            htmlElem.classList.remove("light");
-        }
-        var theme = currentTheme == "dark"
-            ? "light"
-            : "dark";
-        currentTheme = theme;
-        localStorage.setItem("theme", theme);
-
-        updateUI(theme);
-    });
-
-    setTheme(preferredTheme, false);
-    updateUI(preferredTheme);
+  setTheme(preferredTheme, false);
+  updateUI(preferredTheme);
 })();
